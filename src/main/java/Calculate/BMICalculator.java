3818ae445e6a9c458188b4480af9dcf05f86f1e8
BeezyVuitton <jadak777@gmail.com>
2019-03-24 13:30:38 +0100

    secondCommit

---
+++ src/main/java/Calculate/BMICalculator.java
@@ -0,0 +1,18 @@
+package Calculate;
+
+public class BMICalculator implements Calculator {
+
+    public BMICalculator(int height, int weight){
+
+    }
+
+    @Override
+    public String calculate() {
+        return "";
+    }
+
+    @Override
+    public String interpret() {
+        return "";
+    }
+}
---
+++ src/main/java/Calculate/Calculator.java
@@ -0,0 +1,8 @@
+package Calculate;
+
+public interface Calculator {
+    String calculate();
+    String interpret();
+
+
+}
